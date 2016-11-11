202994d1ddf313ddf819753de350ffec77525f8f
Stian Haga <hagastian@gmail.com>
2016-11-11 13:04:30 +0100

    At least its working

--- psapi-data.sketchplugin/Contents/Sketch/script.js
+++ psapi-data.sketchplugin/Contents/Sketch/script.js
@@ -2,26 +2,60 @@ var onRun = function(context) {
   var doc = context.document;
   var selection = context.selection;
 
-  // Ask for some info
-  var options = ['Truncate words', 'Truncate characters']
-  var choices = createDialog('How many lines do you need?', options, 0)
-  var lines = choices[1];
-  var go = choices[0] == 1000 ? true : false;
-  var truncateMode = choices[2];
-
-  if(lines && go) {
-
-    selection.forEach(function(layer){
-      if(isLayerText(layer)) {
-        layer.setStringValue(lines);
-      }
-    });
+  var result = getJSON("http://psapi.nrk.no/medium/tv/plugs?maxnumber=50");
+  var plugs = [];
+  for (var i = 0; i < result.length; i++) {
+    var plug = {
+      image: result[i].image.webImages[5].imageUrl,
+      title: result[i].title
+    };
+    plugs.push(plug);
   }
 
+  for (var j = 0; j < selection.length; j++) {
+    layer = selection[j];
+    if (isLayerText(layer)) {
+      layer.setStringValue(plugs[j].title);
+    } else if (isLayerImage(layer) || isLayerShape(layer)) {
+      var url = getImageFromRemoteURL(plugs[j].image)
+      var imagedata = MSImageData.alloc().initWithImage_convertColorSpace(url, false);
+      var fill = layer.style().fills().firstObject()
+      if (!fill) fill = layer.style().addStylePartOfType(0);
+      fill.setFillType(4);
+      fill.setPatternFillType(1);
+      fill.setIsEnabled(true);
+      fill.setImage(imagedata);
+    }
+  }
 
   // Checks if the layer is a text layer.
   function isLayerText(layer) {
     return layer.isKindOfClass(MSTextLayer.class());
+  }
+
+  function isLayerImage(layer) {
+    return layer.isKindOfClass(MSBitmapLayer.class());
+  }
+
+  function isLayerShape(layer) {
+    return layer.isKindOfClass(MSShapeGroup.class());
+  }
+
+  function getJSON(url) {
+    var request = NSURLRequest.requestWithURL(NSURL.URLWithString(url));
+    var response = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, null, null);
+    return JSON.parse(NSString.alloc().initWithData_encoding(response, NSUTF8StringEncoding));
+  }
+
+  function getImageFromRemoteURL(urlString) {
+    //get data from url
+    var url = NSURL.URLWithString(urlString)
+    var data = url.resourceDataUsingCache(false)
+    if (!data) return
+
+    //create image from data
+    var image = NSImage.alloc().initWithData(data)
+    return image
   }
 
   // Refreshes text layer boundaries after setting text.
@@ -33,30 +67,4 @@ var onRun = function(context) {
     layer.setIsEditingText(false);
     layer.select_byExpandingSelection(false, false);
   }
-
-  function createDialog(msg, items, selectedItemIndex) {
-    selectedItemIndex = selectedItemIndex || 0
-
-    var accessoryInput = NSView.alloc().initWithFrame(NSMakeRect(0,0,300,25));
-    var input = NSTextField.alloc().initWithFrame(NSMakeRect(0,0,300,25));
-    input.editable = true;
-    input.stringValue = '2';
-    accessoryInput.addSubview(input);
-
-    var accessoryList = NSComboBox.alloc().initWithFrame(NSMakeRect(0,0,160,25));
-    accessoryList.addItemsWithObjectValues(items);
-    accessoryList.selectItemAtIndex(selectedItemIndex);
-
-    var alert = COSAlertWindow.alloc().init();
-    alert.setMessageText(msg);
-    alert.addButtonWithTitle('OK');
-    alert.addButtonWithTitle('Cancel');
-    alert.addAccessoryView(accessoryInput);
-    alert.addAccessoryView(accessoryList);
-
-    var responseCode = alert.runModal();
-
-    return [responseCode, input.stringValue(), accessoryList.indexOfSelectedItem()];
-  }
-
 }
